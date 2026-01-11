ab3520416c84f89ff94b8e06f6549221b89b317c
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-11 17:56:23 +0100

    data needs to wait for resource existence

--- terraform/deploy/data.tf
+++ terraform/deploy/data.tf
@@ -1,6 +1,8 @@
 # This file contains the data sources that are used in the Terraform configuration.
 data "digitalocean_kubernetes_cluster" "kronos" {
   name = digitalocean_kubernetes_cluster.kronos.name
+
+  depends_on = [digitalocean_kubernetes_cluster.kronos]
 }
 
 data "kubernetes_service_v1" "nginx_ingress" {
@@ -8,4 +10,6 @@ data "kubernetes_service_v1" "nginx_ingress" {
     name      = "ingress-nginx-controller"
     namespace = "kube-system"
   }
+
+  depends_on = [module.nginx-controller]
 }
