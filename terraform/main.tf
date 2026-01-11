51b02b87a56e3056a9266a3d6562d90b08b396c2
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-11 18:18:28 +0100

    Move project resource to deploy dir

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,32 +2,6 @@
 
 resource "random_pet" "kronos" {}
 
-resource "digitalocean_project" "kronos" {
-  name        = "kronos-project"
-  description = "Kronos World Clock Project"
-  purpose     = "Class project / Educational purposes"
-  environment = "Development"
-  resources = [
-    digitalocean_tag.kronos.urn,
-    digitalocean_vpc.kronos.urn,
-    digitalocean_firewall.kronos.urn,
-    digitalocean_vpc_nat_gateway.kronos.urn,
-    digitalocean_firewall.kronos_bastion.urn,
-    digitalocean_droplet.kronos.urn,
-    digitalocean_kubernetes_cluster.kronos.urn
-  ]
-
-  depends_on = [
-    digitalocean_tag.kronos,
-    digitalocean_vpc.kronos,
-    digitalocean_firewall.kronos,
-    digitalocean_vpc_nat_gateway.kronos,
-    digitalocean_firewall.kronos_bastion,
-    digitalocean_droplet.kronos,
-    digitalocean_kubernetes_cluster.kronos
-  ]
-}
-
 resource "digitalocean_tag" "kronos" {
   name = "kronos"
 }
