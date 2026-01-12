a4b43081c9d243a8f2dc798e829f891e664084cb
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-12 12:54:57 +0100

    install kubectl

--- terraform/deploy/ingress.tf
+++ terraform/deploy/ingress.tf
@@ -13,6 +13,16 @@ resource "null_resource" "wait_for_ingress_webhook" {
       mv doctl $HOME/bin/
       export PATH=$HOME/bin:$PATH
 
+      apt-get update
+      apt-get install -y apt-transport-https ca-certificates curl gnupg
+      curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+      chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+      echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+      chmod 644 /etc/apt/sources.list.d/kubernetes.list
+
+      apt-get update
+      apt-get install -y kubectl
+
       doctl auth init -t ${var.do_token}
 
       echo "Waiting for ingress-nginx-controller DaemonSet pods to be ready..."
