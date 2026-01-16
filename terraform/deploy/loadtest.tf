1cc640e7cbc2925345792e55e2496c31fd55eec4
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-16 19:30:56 +0100

    Use TestRun CRD instead of K6 CRD

--- terraform/deploy/loadtest.tf
+++ terraform/deploy/loadtest.tf
@@ -3,7 +3,7 @@ resource "helm_release" "k6_operator" {
   name             = "k6-operator"
   repository       = "https://grafana.github.io/helm-charts"
   chart            = "k6-operator"
-#   namespace        = "k6-operator-system"
+  namespace        = "k6-operator-system"
   create_namespace = true
 
   wait = true
