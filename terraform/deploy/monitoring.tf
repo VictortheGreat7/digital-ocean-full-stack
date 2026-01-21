c7b7f0d2ec284671381a7c09d00fd219c2e79741
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-21 11:19:33 +0100

    Fix invalid alloy syntax

--- terraform/deploy/monitoring.tf
+++ terraform/deploy/monitoring.tf
@@ -311,113 +311,16 @@ resource "helm_release" "alloy" {
       alloy = {
         configMap = {
           content = <<-EOT
-            // local.file_match discovers files on the local filesystem using glob patterns and the doublestar library. It returns an array of file paths.
-            local.file_match "node_logs" {
-              path_targets = [{
-                  // Monitor syslog to scrape node-logs
-                  __path__  = "/var/log/syslog",
-                  job       = "node/syslog",
-                  node_name = sys.env("HOSTNAME"),
-                  cluster   = "optimum-panther-cluster",
-              }]
-            }
-
-            // loki.source.file reads log entries from files and forwards them to other loki.* components.
-            // You can specify multiple loki.source.file components by giving them different labels.
-            loki.source.file "node_logs" {
-              targets    = local.file_match.node_logs.targets
-              forward_to = [loki.write.loki.receiver]
-            }
-
             // discovery.kubernetes allows you to find scrape targets from Kubernetes resources.
-            // It watches cluster state and ensures targets are continually synced with what is currently running in your cluster.
-            discovery.kubernetes "pod" {
+            // It watches cluster state and ensures targets are continually synced with what is currently running in your cluste  
+            discovery.kubernetes "pods" {
               role = "pod"
-              // Restrict to pods on the node to reduce cpu & memory usage
-              selectors {
-                role = "pod"
-                field = "spec.nodeName=" + coalesce(sys.env("HOSTNAME"), constants.hostname)
-              }
-            }
-
-            // discovery.relabel rewrites the label set of the input targets by applying one or more relabeling rules.
-            // If no rules are defined, then the input targets are exported as-is.
-            discovery.relabel "pod_logs" {
-              targets = discovery.kubernetes.pod.targets
-
-              // Label creation - "namespace" field from "__meta_kubernetes_namespace"
-              rule {
-                source_labels = ["__meta_kubernetes_namespace"]
-                action = "replace"
-                target_label = "namespace"
-              }
-
-              // Label creation - "pod" field from "__meta_kubernetes_pod_name"
-              rule {
-                source_labels = ["__meta_kubernetes_pod_name"]
-                action = "replace"
-                target_label = "pod"
-              }
-
-              // Label creation - "container" field from "__meta_kubernetes_pod_container_name"
-              rule {
-                source_labels = ["__meta_kubernetes_pod_container_name"]
-                action = "replace"
-                target_label = "container"
-              }
-
-              // Label creation -  "app" field from "__meta_kubernetes_pod_label_app_kubernetes_io_name"
-              rule {
-                source_labels = ["__meta_kubernetes_pod_label_app_kubernetes_io_name"]
-                action = "replace"
-                target_label = "app"
-              }
-
-              // Label creation -  "job" field from "__meta_kubernetes_namespace" and "__meta_kubernetes_pod_container_name"
-              // Concatenate values __meta_kubernetes_namespace/__meta_kubernetes_pod_container_name
-              rule {
-                source_labels = ["__meta_kubernetes_namespace", "__meta_kubernetes_pod_container_name"]
-                action = "replace"
-                target_label = "job"
-                separator = "/"
-                replacement = "$1"
-              }
-
-              // Label creation - "__path__" field from "__meta_kubernetes_pod_uid" and "__meta_kubernetes_pod_container_name"
-              // Concatenate values __meta_kubernetes_pod_uid/__meta_kubernetes_pod_container_name.log
-              rule {
-                source_labels = ["__meta_kubernetes_pod_uid", "__meta_kubernetes_pod_container_name"]
-                action = "replace"
-                target_label = "__path__"
-                separator = "/"
-                replacement = "/var/log/pods/*$1/*.log"
-              }
-
-              // Label creation -  "container_runtime" field from "__meta_kubernetes_pod_container_id"
-              rule {
-                source_labels = ["__meta_kubernetes_pod_container_id"]
-                action = "replace"
-                target_label = "container_runtime"
-                regex = "^(\\S+):\\/\\/.+$"
-                replacement = "$1"
               }
             }
 
             // loki.source.kubernetes tails logs from Kubernetes containers using the Kubernetes API.
             loki.source.kubernetes "pod_logs" {
-              targets    = discovery.relabel.pod_logs.output
-              forward_to = [loki.process.pod_logs.receiver]
-            }
-
-            // loki.process receives log entries from other Loki components, applies one or more processing stages,
-            // and forwards the results to the list of receivers in the component's arguments.
-            loki.process "pod_logs" {
-              stage.static_labels {
-                  values = {
-                    cluster = "optimum-panther-cluster",
-                  }
-              }
-
+              targets    = discovery.kubernetes.pods.target
               forward_to = [loki.write.loki.receiver]
             }
 
@@ -426,9 +329,7 @@ resource "helm_release" "alloy" {
             loki.source.kubernetes_events "cluster_events" {
               job_name   = "integrations/kubernetes/eventhandler"
               log_format = "logfmt"
-              forward_to = [
-                loki.process.cluster_events.receiver,
-              ]
+              forward_to = [loki.process.cluster_events.receiver]
             }
 
             // loki.process receives log entries from other loki components, applies one or more processing stages,
@@ -438,7 +339,7 @@ resource "helm_release" "alloy" {
 
               stage.static_labels {
                 values = {
-                  cluster = "optimum-panther-cluster",
+                  cluster = "glad-colt-cluster",
                 }
               }
 
@@ -446,6 +347,13 @@ resource "helm_release" "alloy" {
                 values = {
                   kubernetes_cluster_events = "job",
                 }
+              }
+            }
+
+            // Write logs to Loki
+            loki.write "loki" {
+              endpoint {
+                url = "http://loki.monitoring:3100/loki/api/v1/push"
               }
             }
 
@@ -457,16 +365,12 @@ resource "helm_release" "alloy" {
                 endpoint = "0.0.0.0:4318"
               }
               output {
-                metrics = [otelcol.processor.batch.default.input]
-                logs = [otelcol.processor.batch.default.input]
                 traces = [otelcol.processor.batch.default.input]
               }
             }
             
             otelcol.processor.batch "default" {
                 output {
-                  metrics = [otelcol.exporter.otlp.tempo.input]
-                  logs    = [otelcol.exporter.otlp.tempo.input]
                   traces  = [otelcol.exporter.otlp.tempo.input]
                 }
             }
@@ -483,17 +387,10 @@ resource "helm_release" "alloy" {
             otelcol.service "traces" {
               pipelines {
                 traces = {
-                  receivers = [otelcol.receiver.otlp.default]
-                  processors = [otelcol.processor.batch.default]
-                  exporters = [otelcol.exporter.otlp.tempo]
+                  receivers = [otelcol.receiver.otlp.default],
+                  processors = [otelcol.processor.batch.default],
+                  exporters = [otelcol.exporter.otlp.tempo],
                 }
-              }
-            }
-
-            // Write logs to Loki
-            loki.write "loki" {
-              endpoint {
-                url = "http://loki.monitoring:3100/loki/api/v1/push"
               }
             }
           EOT
