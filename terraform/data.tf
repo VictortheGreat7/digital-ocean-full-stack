29eb3bf41ed07fc1c53f8062a1101dc91dd3cf71
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-02-05 15:07:18 +0100

    enableGatewayAPI for certmanager

--- terraform/data.tf
+++ terraform/data.tf
@@ -10,7 +10,7 @@ data "digitalocean_kubernetes_cluster" "kronos" {
 data "kubernetes_service_v1" "cilium_gateway" {
   metadata {
     name      = "cilium-gateway-kronos"
-    namespace = "cert-manager"
+    namespace = "kube-system"
   }
 
   depends_on = [kubernetes_manifest.cilium_gateway]
