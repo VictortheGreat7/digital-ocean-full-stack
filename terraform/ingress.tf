2ce46fd2ebef43ad1db866a3540bdbccdc1b27f0
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-02-06 12:36:38 +0100

    add http redirect

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -147,3 +147,50 @@ resource "kubernetes_manifest" "kronos_https_route" {
     kubernetes_manifest.cilium_gateway
   ]
 }
+
+resource "kubernetes_manifest" "kronos_http_route" {
+  manifest = {
+    apiVersion = kubernetes_manifest.cilium_gateway.manifest["apiVersion"]
+    kind       = "HTTPRoute"
+    metadata = {
+      name      = "kronos-http-route"
+      namespace = kubernetes_namespace_v1.kronos.metadata[0].name
+    }
+    spec = {
+      parentRefs = [
+        {
+          name = kubernetes_manifest.cilium_gateway.manifest["metadata"]["name"]
+          sectionName = "http"
+        }
+      ]
+      hostnames = ["${var.subdomains[0]}.${var.domain}"]
+      rules = [
+        {
+          matches = [
+            {
+              path = {
+                type  = "PathPrefix"
+                value = "/"
+              }
+            }
+          ]
+          filters = [
+            {
+              type = "RequestRedirect"
+              requestRedirect = {
+                scheme = "https"
+                statusCode   = 301
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+
+  depends_on = [
+    kubernetes_service_v1.kronos_frontend,
+    kubernetes_service_v1.kronos_backend,
+    kubernetes_manifest.cilium_gateway
+  ]
+}
