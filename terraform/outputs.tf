54dbc942cef7d68f01437b8fba553883d2b2fb61
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-02-09 13:14:23 +0100

    add cluster name output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,11 @@ output "doks_connect" {
   value = "doctl kubernetes cluster kubeconfig save ${digitalocean_kubernetes_cluster.kronos.name}"
 }
 
+output "doks_cluster_name" {
+  value     = digitalocean_kubernetes_cluster.kronos.name
+  sensitive = true
+}
+
 output "doks_cluster_id" {
   value     = digitalocean_kubernetes_cluster.kronos.id
   sensitive = true
