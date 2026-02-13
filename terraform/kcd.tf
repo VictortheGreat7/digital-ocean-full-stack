b93394a49092603c42a84dc3eebb2176ef8aa608
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-02-13 08:35:00 +0100

    knative cd beginnings

---
+++ terraform/kcd.tf
@@ -0,0 +1,54 @@
+resource "helm_release" "argo_cd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  create_namespace = true
+  atomic           = true
+  cleanup_on_fail  = true
+
+  values = [
+    yamlencode(
+        {
+            server = {
+                httproute = {
+                    enabled = true
+                    parentRefs = [
+                        {
+                            name = "kronos"
+                            namespace = "kube-system"
+                            sectionName = "https"
+                        }
+                    ]
+                    hostnames = ["${var.subdomains[0]}.${var.domain}"]
+                    rules = [
+                        {
+                            path = {
+                                type  = "PathPrefix"
+                                value = "/kcd/argo"
+                            }
+                        }
+                    ]
+                    filters = [
+                        {
+                            type = "URLRewrite"
+                            urlRewrite = {
+                                path = {
+                                    type = "ReplacePrefixMatch"
+                                    replacePrefixMatch = "/"
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    )
+  ]
+
+  wait    = true
+  timeout = 600
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.kronos
+  ]
+}
