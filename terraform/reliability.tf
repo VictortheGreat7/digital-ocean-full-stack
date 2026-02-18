1653c85efea431a6fe8acc5e62bcfdc9ae9c488c
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-02-18 11:59:49 +0100

    spike test

--- terraform/reliability.tf
+++ terraform/reliability.tf
@@ -22,22 +22,22 @@ resource "helm_release" "monitoring_argocd" {
   ]
 }
 
-# resource "helm_release" "chaos_argocd" {
-#   name             = "chaos-argocd"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argocd-apps"
-#   namespace        = "knative-cd"
-#   create_namespace = false
-#   atomic           = true
-#   cleanup_on_fail  = true
+resource "helm_release" "chaos_argocd" {
+  name             = "chaos-argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  namespace        = "knative-cd"
+  create_namespace = false
+  atomic           = true
+  cleanup_on_fail  = true
 
-#   values = [file("${path.root}/terraform-helm/argocd/argocd-apps/chaos-values.yaml")]
+  values = [file("${path.root}/terraform-helm/argocd/argocd-apps/chaos-values.yaml")]
 
-#   wait    = true
-#   timeout = 600
+  wait    = true
+  timeout = 600
 
-#   depends_on = [
-#     helm_release.kronos_argocd,
-#     helm_release.monitoring_argocd
-#   ]
-# }
+  depends_on = [
+    helm_release.kronos_argocd,
+    helm_release.monitoring_argocd
+  ]
+}
