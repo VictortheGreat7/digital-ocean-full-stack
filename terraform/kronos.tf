26f48f7203a93c7eddff4fd5b42b2c661b8c84f4
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-02-25 09:03:02 +0100

    increase prometheus resources

--- terraform/kronos.tf
+++ terraform/kronos.tf
@@ -1,19 +1,19 @@
-resource "helm_release" "kronos_argocd" {
-  name             = "kronos-argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argocd-apps"
-  namespace        = "knative-cd"
-  create_namespace = false
-  atomic           = true
-  cleanup_on_fail  = true
+# resource "helm_release" "kronos_argocd" {
+#   name             = "kronos-argocd"
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argocd-apps"
+#   namespace        = "knative-cd"
+#   create_namespace = false
+#   atomic           = true
+#   cleanup_on_fail  = true
 
-  values = [file("${path.root}/terraform-helm/argocd/argocd-apps/app-values.yaml")]
+#   values = [file("${path.root}/terraform-helm/argocd/argocd-apps/app-values.yaml")]
 
-  wait    = true
-  timeout = 600
+#   wait    = true
+#   timeout = 600
 
-  depends_on = [
-    helm_release.monitoring_argocd,
-    kubernetes_secret_v1.postgres_pass
-  ]
-}
+#   depends_on = [
+#     helm_release.monitoring_argocd,
+#     kubernetes_secret_v1.postgres_pass
+#   ]
+# }
