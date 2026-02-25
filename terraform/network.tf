a86cea3a3b91ed7f635691cca31ff83a039016e5
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-02-25 16:02:58 +0100

    reduce loadtest pods to 4

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,33 +10,27 @@ resource "digitalocean_tag" "kronos" {
   depends_on = [random_pet.kronos]
 }
 
+# trivy:ignore:DIG-0003
 resource "digitalocean_firewall" "kronos" {
   name = "${random_pet.kronos.id}-firewall"
-
   tags = [digitalocean_tag.kronos.name]
 
-  # trivy:ignore:DIG-0003
   inbound_rule {
     protocol         = "tcp"
     port_range       = "443"
     source_addresses = ["0.0.0.0/0"]
-  } # trivy:ignore:DIG-0003
+  }
 
-  # trivy:ignore:DIG-0003
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
     destination_addresses = ["0.0.0.0/0"]
   }
-
-  # trivy:ignore:DIG-0003
   outbound_rule {
     protocol              = "udp"
     port_range            = "1-65535"
     destination_addresses = ["0.0.0.0/0"]
   }
-
-  # trivy:ignore:DIG-0003
   outbound_rule {
     protocol              = "icmp"
     destination_addresses = ["0.0.0.0/0"]
--- terraform/traffic.tf
+++ terraform/traffic.tf
@@ -83,10 +83,10 @@ resource "helm_release" "gateway" {
 }
 
 resource "helm_release" "external_dns" {
-  name       = "external-dns"
-  repository = "https://kubernetes-sigs.github.io/external-dns/"
-  chart      = "external-dns"
-  namespace  = "kube-system"
+  name             = "external-dns"
+  repository       = "https://kubernetes-sigs.github.io/external-dns/"
+  chart            = "external-dns"
+  namespace        = "kube-system"
   create_namespace = false
   atomic           = true
   cleanup_on_fail  = true
