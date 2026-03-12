024d4f9620957354fc1bf1babc51d27a202dd91d
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-03-12 11:51:32 +0100

    disable gevent

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,13 +1,4 @@
 bind = "0.0.0.0:5000"
-
 workers = 4
-worker_class = "gevent"
-worker_connections = 1000
-
-backlog = 2048
-accesslog = None
-keepalive = 5
-timeout = 60
-graceful_timeout = 30
-
+threads = 4
 control_socket_disable = True
